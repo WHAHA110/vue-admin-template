5c07d30073c71c178de9c8d3942ce500d778ac26
#001 <#001@qq.com>
2021-09-29 14:31:04 +0800

    登录 获取菜单；

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+    <div id="app">
+        <router-view />
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+    name: 'App'
 }
 </script>
+<style lang="less" >
+* {
+    font-size: 12px !important;
+}
+.app-container {
+    padding: 9px;
+}
+</style>
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,97 +1,129 @@
-import { login, logout, getInfo } from '@/api/user'
-import { getToken, setToken, removeToken } from '@/utils/auth'
-import { resetRouter } from '@/router'
+import { login, logout, getInfo } from "@/api/user";
+import { getToken, setToken, removeToken } from "@/utils/auth";
+import { resetRouter } from "@/router";
+
+function _import(file) {
+  //   return () => import("@/views/table/index");
+  return () => import("@/views/" + file + ".vue");
+}
+
+function filterRouter(routers) {
+  // 遍历后台传来的路由字符串，转换为组件对象
+  const accessedRouters = routers.filter(route => {
+    if (route.component) {
+      if (route.component === "Layout") {
+        // Layout 组件特殊处理
+        route.component = Layout;
+      } else {
+        route.component = _import(route.component);
+        console.log("@/views/" + file + ".vue", "----------------->filePath");
+        console.log(route.name, route, "----------------->filterRouter");
+      }
+    }
+    if (route.children && route.children.length) {
+      route.children = filterRouter(route.children);
+    }
+    return true;
+  });
+  return accessedRouters;
+}
 
 const getDefaultState = () => {
   return {
+    authRoutes: [],
     token: getToken(),
-    name: '',
-    avatar: ''
-  }
-}
+    name: "",
+    avatar: ""
+  };
+};
 
-const state = getDefaultState()
+const state = getDefaultState();
 
 const mutations = {
-  RESET_STATE: (state) => {
-    Object.assign(state, getDefaultState())
+  RESET_STATE: state => {
+    Object.assign(state, getDefaultState());
   },
   SET_TOKEN: (state, token) => {
-    state.token = token
+    state.token = token;
   },
   SET_NAME: (state, name) => {
-    state.name = name
+    state.name = name;
   },
   SET_AVATAR: (state, avatar) => {
-    state.avatar = avatar
+    state.avatar = avatar;
   }
-}
+};
 
 const actions = {
   // user login
   login({ commit }, userInfo) {
-    const { username, password } = userInfo
+    const { username, password } = userInfo;
     return new Promise((resolve, reject) => {
-      login({ username: username.trim(), password: password }).then(response => {
-        const { data } = response
-        commit('SET_TOKEN', data.token)
-        setToken(data.token)
-        resolve()
-      }).catch(error => {
-        reject(error)
-      })
-    })
+      login({ username: username.trim(), password: password })
+        .then(response => {
+          const { data } = response;
+          commit("SET_TOKEN", data);
+          setToken(data);
+          resolve();
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
   },
 
   // get user info
   getInfo({ commit, state }) {
     return new Promise((resolve, reject) => {
-      getInfo(state.token).then(response => {
-        const { data } = response
+      getInfo(state.token)
+        .then(response => {
+          const { data } = response;
+          state.authRoutes = data;
+          if (!data) {
+            return reject("Verification failed, please Login again.");
+          }
 
-        if (!data) {
-          return reject('Verification failed, please Login again.')
-        }
+          const { name, avatar } = data;
 
-        const { name, avatar } = data
-
-        commit('SET_NAME', name)
-        commit('SET_AVATAR', avatar)
-        resolve(data)
-      }).catch(error => {
-        reject(error)
-      })
-    })
+          commit("SET_NAME", name);
+          commit("SET_AVATAR", avatar);
+          resolve(data);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
   },
 
   // user logout
   logout({ commit, state }) {
     return new Promise((resolve, reject) => {
-      logout(state.token).then(() => {
-        removeToken() // must remove  token  first
-        resetRouter()
-        commit('RESET_STATE')
-        resolve()
-      }).catch(error => {
-        reject(error)
-      })
-    })
+      logout(state.token)
+        .then(() => {
+          removeToken(); // must remove  token  first
+          resetRouter();
+          commit("RESET_STATE");
+          resolve();
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
   },
 
   // remove token
   resetToken({ commit }) {
     return new Promise(resolve => {
-      removeToken() // must remove  token  first
-      commit('RESET_STATE')
-      resolve()
-    })
+      removeToken(); // must remove  token  first
+      commit("RESET_STATE");
+      resolve();
+    });
   }
-}
+};
 
 export default {
   namespaced: true,
   state,
   mutations,
   actions
-}
-
+};
